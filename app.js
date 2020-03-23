48d3a6fc766a7cbba279e32accaa2b1360ab4304
Odomfavour <ogochukwuodom@gmail.com>
2020-03-23 11:54:45 +0100

    moved each message to new line

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var body = `
     <meta http-equiv="Content-Type" content="text/html" charset="UTF-8">
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Node Js Form</title>
 </head>
 <body>
 <style>
