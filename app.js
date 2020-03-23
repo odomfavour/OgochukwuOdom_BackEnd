2ba5364e8df4b42250ab18fb8a236e9958f0d065
Odomfavour <ogochukwuodom@gmail.com>
2020-03-23 11:53:48 +0100

    moved each message to new line

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ var fs = require('fs');
 http
   .createServer(function(req, res) {
 var body = `
-<html>
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html" charset="UTF-8"/>
+    <meta http-equiv="Content-Type" content="text/html" charset="UTF-8">
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <style>
 
     h3, p {
